39114144ee0879e8f6df76a8499136990d02fba2
Tobias Noelkensmeier <Tobiassherco125@gmail.com>
2023-08-07 16:47:01 +0200

    Movement added

---
+++ MoonDust2d/Assets/Code/Movement.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float _Speed = 7.0f;
+    public float _JumpForce = 10.0f;
+    private bool triggered;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A)) {
+            
+            transform.Translate(Vector3.left * Time.deltaTime * _Speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * _Speed);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && triggered)
+        {
+            rb.velocity = Vector2.up * _JumpForce;
+            print("Jump");
+        }
+      
+        print(triggered);
+        
+    }
+
+    private void OnCollisionEnter2D (Collision2D other)
+    {
+        if (other.gameObject.tag == "Platform")
+        {
+            triggered = true;
+        }
+    }
+    private void OnCollisionExit2D (Collision2D other)
+    {
+        if (other.gameObject.tag == "Platform")
+        {
+            triggered = false;
+        }
+    }
+}
