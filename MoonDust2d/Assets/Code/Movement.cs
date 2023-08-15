a0bae0a5d832dcb05b9a2ea8dc4ef81052d3dfb4
Tobias Noelkensmeier <Tobiassherco125@gmail.com>
2023-08-15 23:11:23 +0200

    Melee combat added(Without enemies)
    This is just for the player to be able to click to spawn melee hit box limited times per second

--- MoonDust2d/Assets/Code/Movement.cs
+++ MoonDust2d/Assets/Code/Movement.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Movement : MonoBehaviour
 {
@@ -14,8 +15,13 @@ public class Movement : MonoBehaviour
     private bool triggered;
     
     //Animation variables
-    public SpriteRenderer spriteRenderer;
     public Animator animator;
+    
+    //Attack variables
+    public GameObject attackWeapon;
+
+    private float _attackRate = 2f;
+    private float _nextAttackTime = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,9 +31,10 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        print(_nextAttackTime);
         //Movement of the player
         Move(); 
-        
+        attack();
         //Test if the player is on the ground
         //print(triggered);
         
@@ -54,18 +61,19 @@ public class Movement : MonoBehaviour
         if(Input.GetKey(KeyCode.A)) {
             
             transform.Translate(Vector3.left * Time.deltaTime * _Speed);
-            spriteRenderer.flipX = true;
+            transform.localScale = new Vector3(-1, 1, 1);
             animator.SetBool("Moving", true);
         }
         //Check if the Any key is pressed to change the animation back to idle
         else if (Input.GetKey(KeyCode.A) == false)
         {
             animator.SetBool("Moving", false);
+            
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector3.right * Time.deltaTime * _Speed);
-            spriteRenderer.flipX = false;
+            transform.localScale = new Vector3(1, 1, 1);
             animator.SetBool("Moving", true);
         }
         if (Input.GetKeyDown(KeyCode.Space) && triggered)
@@ -73,6 +81,23 @@ public class Movement : MonoBehaviour
             rb.velocity = Vector2.up * _JumpForce;
             print("Jump");
             animator.SetBool("Moving", true);
+        }
+        
+    }
+    void attack()
+    {
+        if(Time.time >= _nextAttackTime)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                _nextAttackTime = Time.time + 1f / _attackRate;
+                attackWeapon.SetActive(true);
+                print("Attack");
+            }
+            else
+            {
+                attackWeapon.SetActive(false);
+            }
         }
     }
 }
