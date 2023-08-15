db035a59b9fe6ef6ceba8c8a518d09df06d815f9
Tobias Noelkensmeier <Tobiassherco125@gmail.com>
2023-08-15 14:24:05 +0200

    Player Animation transition added(Need idle animation images)

--- MoonDust2d/Assets/Code/Movement.cs
+++ MoonDust2d/Assets/Code/Movement.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    //physics variables
     public Rigidbody2D rb;
     public float _Speed = 7.0f;
     public float _JumpForce = 10.0f;
+    
+    //Grounded variables
     private bool triggered;
+    
+    //Animation variables
     public SpriteRenderer spriteRenderer;
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,27 +25,15 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.A)) {
-            
-            transform.Translate(Vector3.left * Time.deltaTime * _Speed);
-            spriteRenderer.flipX = true;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * Time.deltaTime * _Speed);
-            spriteRenderer.flipX = false;
-        }
-        if (Input.GetKeyDown(KeyCode.Space) && triggered)
-        {
-            rb.velocity = Vector2.up * _JumpForce;
-            print("Jump");
-        }
-      
-        print(triggered);
+        //Movement of the player
+        Move(); 
+        
+        //Test if the player is on the ground
+        //print(triggered);
         
     }
-
+    
+    //Test if the player is on the ground
     private void OnCollisionEnter2D (Collision2D other)
     {
         if (other.gameObject.tag == "Platform")
@@ -52,6 +46,28 @@ public class Movement : MonoBehaviour
         if (other.gameObject.tag == "Platform")
         {
             triggered = false;
+        }
+    }
+    //Movement of the player
+    public void Move()
+    {
+        if(Input.GetKey(KeyCode.A)) {
+            
+            transform.Translate(Vector3.left * Time.deltaTime * _Speed);
+            spriteRenderer.flipX = true;
+            animator.SetBool("Moving", true);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * _Speed);
+            spriteRenderer.flipX = false;
+            animator.SetBool("Moving", true);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && triggered)
+        {
+            rb.velocity = Vector2.up * _JumpForce;
+            print("Jump");
+            animator.SetBool("Moving", true);
         }
     }
 }
