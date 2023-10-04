d436be61203f2b3955efd9ecbfe7459b01210988
Tobias Noelkensmeier <Tobiassherco125@gmail.com>
2023-10-04 18:47:27 +0200

    Sound frame work added and arlo fire added

---
+++ MoonDust2d/Assets/Code/SoundManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    public AudioClip[] audioClips;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void JumpSound()
+    {
+        _audioSource.PlayOneShot(audioClips[0]);
+    }
+    public void MeleeSound()
+    {
+        _audioSource.PlayOneShot(audioClips[1]);
+    }
+    public void WalkSound()
+    {
+        _audioSource.PlayOneShot(audioClips[2]);
+    }
+}
