67ffec8764bd3d7ea8eff6737b0ddd62484891be
Tobias Noelkensmeier <Tobiassherco125@gmail.com>
2023-10-06 12:26:51 +0200

    Added Pause menu with sound volume slider

---
+++ MoonDust2d/Assets/Code/UI Code/PauseMenu.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class PauseMenu : MonoBehaviour
+{ public GameObject pauseMenuUI;
+    public Button resumeButton;
+    private bool isPaused = false;
+    private AudioSource _audioSource;
+    void Start()
+    {
+        // Hide the pause menu at the start of the game
+        pauseMenuUI.SetActive(false);
+        _audioSource = GetComponent<AudioSource>();
+    }
+    
+
+    public void TogglePauseMenu()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            Time.timeScale = 0f; // Pause the game
+            pauseMenuUI.SetActive(true);
+            resumeButton.Select();
+        }
+        else
+        {
+            Time.timeScale = 1f; // Resume the game
+            pauseMenuUI.SetActive(false);
+        }
+    }
+
+    public void ResumeGame()
+    {
+        TogglePauseMenu();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        print("Quit");
+    }
+
+    public void PauseButtonInput(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            TogglePauseMenu();
+        }
+    }
+    public void SetSoundVolume(float volume)
+    {
+        _audioSource.volume = volume;
+    }
+}
