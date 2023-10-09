1bce2dca9d4d972665a0b40aec04efa13cd233ca
Tobias Noelkensmeier <Tobiassherco125@gmail.com>
2023-10-09 12:28:59 +0200

    Sound code made more efficient and attached pause stuff in  lvl2

--- MoonDust2d/Assets/Code/SoundManager.cs
+++ MoonDust2d/Assets/Code/SoundManager.cs
@@ -1,20 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    public enum  SoundState
+    {
+        Silent,
+        Jumping,
+        Melee,
+        
+    }
+    
     private AudioSource _audioSource;
     public AudioClip[] audioClips;
+    
+    public SoundState currentState;
     // Start is called before the first frame update
     void Awake()
     {
         _audioSource = GetComponent<AudioSource>();
     }
-    
+
+    private void Update()
+    {
+        SetState(currentState);
+    }
+
     public void JumpSound()
     {
         _audioSource.PlayOneShot(audioClips[0]);
+        SetState(SoundState.Silent);
     }
     public void MeleeSound()
     {
@@ -23,5 +41,23 @@ public class SoundManager : MonoBehaviour
     public void WalkSound()
     {
         _audioSource.PlayOneShot(audioClips[2]);
+    }
+
+    public void SetState(SoundState newState)
+    {
+        if (currentState != newState)
+        {
+            currentState = newState;
+
+            switch (currentState)
+            {
+                case SoundState.Jumping:
+                    JumpSound();
+                    break;
+                case SoundState.Melee:
+                    MeleeSound();
+                    break;
+            }
+        }
     }
 }
