4855d68dab8ebd9a453259edf46e6f4bd1a955ee
Arlo Rutherford <39-127577@sae.edu.za>
2023-11-19 11:55:49 +0200

    Finished main menu

---
+++ MoonDust2d/Assets/Sounds/References for some sound.cs
@@ -0,0 +1,9 @@
+
+//Below are the references for some sounds that we have sampled/used. All are royalty free and accessed through the links provided. Some sounds were made by myself, Lara Rutherford, and made using sampling of other sounds using Audacity.
+
+
+
+// Main menu/end credits/tutorial music: Spencer_YK. (N.D). "Little slime's adventure". Avaliable at: https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=151007
+//Cutscene sound effects by “Pixabay”.
+
+//Pixabay - Pixabay. (n.d.).Available at: https://pixabay.com/users/pixabay-1/
